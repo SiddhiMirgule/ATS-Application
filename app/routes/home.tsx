b07dec5dec6db3b0aa076a512c429d27fb1a9df7
SiddhiM <siddhimirgule18@gmail.com>
2026-02-27 23:46:06 +0530

    Configured Zustand for lightweight state management

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,36 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Navbar from "~/components/Navbar";
+import {resumes} from "../../constants";
+import {resume} from "react-dom/server";
+import {callbackify} from "node:util";
+import ResumeCard from "~/components/ResumeCard";
+//import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "ATS Application" },
+    { name: "description", content: "Smart Feedback for your Dream Job! " },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className= "bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar/>
+<section className="mainsection">
+  <div className="page-heading">
+    <h1>Track Your Application & Resume Ratings</h1>
+    <h2>Review your submissions and check AI-powered feedback </h2>
+
+  </div>
+
+</section>
+    {resumes.length > 0 && (
+        <div className="resumes-section">
+
+    {resumes.map((resume) =>(
+        <ResumeCard key={resume.id} resume={resume}/>
+    ))}
+        </div>
+    )}
+  </main>
 }
